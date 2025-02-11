baef9eef5b8d62792ead8135105619c86a486589
jo0sel <jo0sel094@gmail.com>
2025-02-11 09:54:14 -0500

    controlador modelo JL

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -10,7 +10,7 @@ const ProductoController = {
             }
         });
     },
-
+//social
     obtenerProductoPorId: (req, res) => {
         const { id } = req.params;
         Producto.obtenerPorId(id, (err, result) => {
