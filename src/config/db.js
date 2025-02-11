811ad33495afc6b1860d05557f8e21c79354d5e8
jo0sel <jo0sel094@gmail.com>
2025-02-11 09:47:03 -0500

    nombre de la base de datos Jose

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'venta_ropa',
+    database: 'venta_ropa',//nombre de base de datos
     port: '3307'
 });
 
